44f61b6aa141f95907a24f05c17259c67af5758f
cqcdsgf <cqcdsgf@163.com>
2017-11-02 17:05:37 +0800

    临时

--- app/base-web/src/main/java/com/sgf/app/example/ExampleController.java
+++ app/base-web/src/main/java/com/sgf/app/example/ExampleController.java
@@ -1,9 +1,17 @@
 package com.sgf.app.example;
 
+import com.sgf.app.example.message.Message;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 /**
  * Created by sgf on 2017/11/2.
@@ -20,8 +28,15 @@ public class ExampleController {
 
     @GetMapping(value = "testDate")
     public ModelAndView testDate(){
-
         return new ModelAndView("example/datetime","message","test");
-
     }
+
+
+    @PostMapping(value = "/testPostDate")
+    public ModelAndView testPostDate(HttpServletRequest request, String choosedDate,String currentDate,  RedirectAttributes redirect){
+
+        return new ModelAndView("example/datetime","choosedDate","2017-11-11");
+    }
+
+
 }
