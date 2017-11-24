d3f42a5eaeda3d86db35e94187a278cee847c7e5
cqcdsgf <cqcdsgf@163.com>
2017-11-24 17:04:27 +0800

    测试plupload后台功能（末完成）

--- app/base-web/src/main/java/com/sgf/app/example/ExampleController.java
+++ app/base-web/src/main/java/com/sgf/app/example/ExampleController.java
@@ -1,23 +1,30 @@
 package com.sgf.app.example;
 
 import com.sgf.app.example.message.Message;
+import com.sgf.base.utils.plupload.Plupload;
+import com.sgf.base.utils.plupload.PluploadUtil;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by sgf on 2017/11/2.
  */
 @Controller
 public class ExampleController {
+    public static final String FileDir = "uploadfile/";
 
     @GetMapping(value = "demoValidate")
     public ModelAndView demoValidate(){
@@ -51,5 +58,41 @@ public class ExampleController {
     @GetMapping(value = "demoUploadImg")
     public ModelAndView demoUploadImg(){
         return new ModelAndView("example/demo-upload-img","message","test");
+    }
+
+    @GetMapping(value = "demoUpload")
+    public ModelAndView demoUpload(){
+        return new ModelAndView("example/demo-plupload","message","test");
+    }
+
+    /**上传处理方法*/
+    @PostMapping(value="/upload")
+    public String upload(Plupload plupload, HttpServletRequest request, HttpServletResponse response) {
+
+        //System.out.println(plupload.getChunk() + "===" + plupload.getName() + "---" + plupload.getChunks());
+
+        plupload.setRequest(request);
+        //文件存储路径
+        File dir = new File(plupload.getRequest().getSession().getServletContext().getRealPath("/") + FileDir);
+
+        System.out.println(dir.getPath());
+
+        try {
+            //上传文件
+            PluploadUtil.upload(plupload, dir);
+            //判断文件是否上传成功（被分成块的文件是否全部上传完成）
+            if (PluploadUtil.isUploadFinish(plupload)) {
+                System.out.println(plupload.getName() + "----");
+            }
+
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return "login.upload";
     }
 }
