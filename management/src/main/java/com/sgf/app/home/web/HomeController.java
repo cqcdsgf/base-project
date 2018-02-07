17c98498d95395ee1ce400a90a6b576e0beae210
cqcdsgf <cqcdsgf@163.com>
2018-02-07 09:57:56 +0800

    格式

--- management/src/main/java/com/sgf/app/home/web/HomeController.java
+++ management/src/main/java/com/sgf/app/home/web/HomeController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "/home/home";
     }
 
