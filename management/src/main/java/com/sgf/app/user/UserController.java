08e3546d0576af70323c365228d8fa602c25e556
cqcdsgf <cqcdsgf@163.com>
2018-02-08 23:00:23 +0800

    创建用户

--- management/src/main/java/com/sgf/app/user/UserController.java
+++ management/src/main/java/com/sgf/app/user/UserController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +35,7 @@ public class UserController {
     private AuthUserService authUserService;
 
     @GetMapping("/user/toUpdate")
-    public ModelAndView toUpdate(){
+    public ModelAndView toUpdate() {
         AuthUser authUser = new AuthUser();
         authUser.setId(1L);
         authUser.setUsername("测试用户名");
@@ -63,61 +65,48 @@ public class UserController {
         authUser.getRoles().add(authRole3);
 
         Map<String, Object> model = Maps.newHashMap();
-        model.put("authUser",authUser);
+        model.put("authUser", authUser);
 
-        return new ModelAndView("user/userUpdate",model);
+        return new ModelAndView("user/userUpdate", model);
     }
 
     @PostMapping("/user/update")
-    public String toUpdate(AuthUser authUser, RedirectAttributes redirectAttributes){
+    public String toUpdate(AuthUser authUser, RedirectAttributes redirectAttributes) {
 
-        redirectAttributes.addFlashAttribute(BaseMessageConstant.MESSAGE,"用户修改成功！");
+        redirectAttributes.addFlashAttribute(BaseMessageConstant.MESSAGE, "用户修改成功！");
         return "redirect:/user/toQueryList";
     }
 
 
     @GetMapping("/user/toQueryList")
-    public ModelAndView toQueryList(){
+    public ModelAndView toQueryList() {
 
         return new ModelAndView("/user/userList");
     }
 
     @GetMapping("/user/toCreate")
-    public ModelAndView toCreate(){
+    public ModelAndView toCreate() {
         AuthUser authUser = new AuthUser();
 
-       List<AuthRole> authRoles = authRoleService.findAll();
+        List<AuthRole> authRoles = authRoleService.findAll();
         authUser.getRoles().addAll(authRoles);
 
-/*       for (AuthRole authRole:authRoles){
-           authRole.getUsers().clear();
-           authUser.getRoles().add(authRole);
-       }*/
-/*        AuthRole authRole2 = new AuthRole();
-        authRole2.setId(2L);
-        authRole2.setSelected(false);
-        authRole2.setName("name2");
-        authRole2.setSummary("测试2");
-        authRole2.setCreateTime(new Date());
-        authRole2.setModifyTime(new Date());
-
-        authUser.getRoles().add(authRole2);*/
-
-
-        Map<String, Object> model = Maps.newHashMap();
-        model.put("authUser",authUser);
-
-        return new ModelAndView("/user/userCreate",model);
+        return new ModelAndView("/user/userCreate", "authUser", authUser);
     }
 
 
     @PostMapping("/user/create")
-    public ModelAndView createUser(AuthUser authUser,Long[] roles){
+    public String createUser(AuthUser authUser, @RequestParam("roleIds") List<Long> roleIds,RedirectAttributes redirectAttributes) {
+        authUser.getRoles().clear();
 
+        List<AuthRole> authRoles =  authRoleService.findAll(roleIds);
 
-        return new ModelAndView("/user/toQueryList",BaseMessageConstant.MESSAGE,"成功创建用户！");
+        authUser.getRoles().addAll(authRoles);
+        authUserService.save(authUser);
+
+        redirectAttributes.addFlashAttribute(BaseMessageConstant.MESSAGE, "成功创建用户！");
+        return "redirect:/user/toQueryList";
     }
-
 
 
 }
