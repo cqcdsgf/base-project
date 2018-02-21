8345268801d1d17ac5d9740e8e2406616402438e
cqcdsgf <cqcdsgf@163.com>
2018-02-21 10:31:14 +0800

    样式显示

--- management/src/main/java/com/sgf/app/permission/AuthPermissionController.java
+++ management/src/main/java/com/sgf/app/permission/AuthPermissionController.java
@@ -2,17 +2,14 @@ package com.sgf.app.permission;
 
 import com.google.common.collect.Maps;
 import com.sgf.app.domain.AuthPermission;
-import com.sgf.app.domain.AuthUser;
 import com.sgf.base.constant.BaseMessageConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,43 +34,80 @@ public class AuthPermissionController {
         return new ModelAndView("/permission/permissionList", "permissions", permissions);
     }
 
-    @ResponseBody
+    @GetMapping("/permission/toCreate")
+    public ModelAndView toCreate() {
+        AuthPermission permission = new AuthPermission();
+
+        return new ModelAndView("/permission/permissionCreate", "permission", permission);
+    }
+
     @PostMapping("/permission/create")
-    public Map<String,Object> create( @RequestParam String name, @RequestParam String value, @RequestParam String url, @RequestParam String summary) {
-        AuthPermission authPermission = new AuthPermission();
-        authPermission.setName(name);
-        authPermission.setValue(value);
-        authPermission.setUrl(url);
-        authPermission.setSummary(summary);
+    public String createRole(AuthPermission authPermission, RedirectAttributes redirectAttributes) {
         authPermissionService.save(authPermission);
 
-        Map<String,Object> result = Maps.newHashMap();
-        result.put(BaseMessageConstant.MESSAGE,"新增权限成功！");
-        return result;
+        redirectAttributes.addFlashAttribute(BaseMessageConstant.MESSAGE, "权限创建成功！");
+        return "redirect:/permission/toQueryList";
     }
 
-    @ResponseBody
+    @GetMapping("/permission/toUpdate")
+    public ModelAndView toUpdate(@RequestParam Long id,@ModelAttribute("errorMessage")String  errorMessage) {
+        Map<String,Object> model = Maps.newHashMap();
+
+        AuthPermission permission = authPermissionService.findOne(id);
+        model.put("permission",permission);
+        model.put(BaseMessageConstant.ERROR_MESSAGE,errorMessage);
+
+        return new ModelAndView("permission/permissionUpdate",model);
+    }
+
     @PostMapping("/permission/update")
-    public Map<String,Object> update(@RequestParam long id, @RequestParam String name, @RequestParam String value, @RequestParam String url, @RequestParam String summary) {
-        AuthPermission authPermission = authPermissionService.findOne(id);
-        authPermission.setName(name);
-        authPermission.setValue(value);
-        authPermission.setUrl(url);
-        authPermission.setSummary(summary);
-
+    public String update(AuthPermission authPermission,RedirectAttributes redirectAttributes) {
         authPermissionService.save(authPermission);
 
-        Map<String,Object> result = Maps.newHashMap();
-        result.put(BaseMessageConstant.MESSAGE,"修改权限成功！");
-        return result;
+        redirectAttributes.addFlashAttribute(BaseMessageConstant.MESSAGE, "权限修改成功！");
+        return "redirect:/permission/toQueryList";
+    }
+
+    @PostMapping("/permission/delete")
+    public String delete(@RequestParam long id,RedirectAttributes redirectAttributes){
+        try {
+            authPermissionService.delete(id);
+        }
+        catch (DataIntegrityViolationException exception){
+            String tip = "删除ID为_"+id + "_的权限时，其它角色拥有此权限，请先解除关联关系后再进行删除权限操作！";
+            logger.error(tip);
+            redirectAttributes.addFlashAttribute(BaseMessageConstant.ERROR_MESSAGE, tip);
+            return "redirect:/permission/toUpdate?id="+id;
+        }
+        catch (Exception exception){
+            String tip = "删除权限时，_ "+id + "_权限删除失败！";
+            logger.error(tip);
+            return "redirect:/permission/toUpdate?id="+id;
+        }
+
+        return "redirect:/permission/toQueryList";
     }
 
     @ResponseBody
-    @PostMapping("/permission/delete")
-    public Map<String,Object> delete(@RequestParam long id){
-        authPermissionService.delete(id);
-
+    @PostMapping("/permission/deleteByAjax")
+    public Map<String,Object> deleteByAjax(@RequestParam long id){
         Map<String,Object> result = Maps.newHashMap();
+        try {
+            authPermissionService.delete(id);
+        }
+        catch (DataIntegrityViolationException exception){
+            String tip = "删除ID为_\"+id + \"_的权限时，其它角色拥有此权限，请先解除关联关系后再进行删除权限操作！";
+            result.put(BaseMessageConstant.ERROR_MESSAGE,tip);
+            logger.error(tip);
+            return result;
+        }
+        catch (Exception exception){
+            String tip = "删除权限时，_ "+id + "_权限删除失败！";
+            result.put(BaseMessageConstant.ERROR_MESSAGE,tip);
+            logger.error(tip);
+            return result;
+        }
+
         result.put(BaseMessageConstant.MESSAGE,"删除权限成功！");
         return result;
     }
